6031dc938ab95471260ad3b61b14ae69bf65da28
PaulBrimley <paul.k.brimley@gmail.com>
2016-01-05 22:54:01 -0700

    completed through step 5

--- js/teams/teamCtrl.js
+++ js/teams/teamCtrl.js
@@ -1,5 +1,35 @@
 var app = angular.module('nbaRoutes');
 // the resolved data from the router needs to be injected into the controller
-app.controller('teamCtrl', function ($scope, $stateParams, teamService) {
+app.controller('teamCtrl', function ($scope, $stateParams, teamService, teamData) {
+
+	$scope.teamData = teamData;
+	$scope.newGame = false;
+
+	$scope.toggleNewGameForm = function() {
+		$scope.showNewGameForm = !$scope.showNewGameForm;
+	}
+
+	if($stateParams.team === 'utahjazz') {
+		$scope.homeTeam = 'Utah Jazz';
+		$scope.logoPath = '/images/jazz-logo.png';
+	} else if ($stateParams.team === 'losangeleslakers') {
+		$scope.homeTeam = 'Los Angeles Lakers';
+		$scope.logoPath = '/images/lakers-logo.png';	
+	} else if ($stateParams.team === 'miamiheat') {
+		$scope.homeTeam = 'Miami Heat';
+		$scope.logoPath = '/images/heat-logo.png';	
+	} 
+
+	$scope.submitGame = function(newGame) {
+		$scope.homeTeamHolder = $scope.homeTeam.split(' ').join('').toLowerCase();
+		newGame.homeTeam = $scope.homeTeamHolder;
+		teamService.addNewGame(newGame).then(function() {
+			teamService.getTeamData(newGame.homeTeam).then(function(data) {
+				$scope.teamData = data;
+				$scope.newGame = {};
+				$scope.showNewGameForm = false;
+			});
+		});
+	}
 
 });
--- js/teams/teamService.js
+++ js/teams/teamService.js
@@ -2,6 +2,38 @@ var app = angular.module('nbaRoutes');
 
 app.service('teamService', function ($http, $q) {
 
-    // service code
+	this.addNewGame = function(newGame) {
+
+		var url = 'https://api.parse.com/1/classes/' + newGame.homeTeam;
+		if (parseInt(newGame.homeTeamScore) > parseInt(newGame.opponentScore)) {
+			newGame.won = true;
+		} else {
+			newGame.won = false;
+		}
+		return $http.post(url, newGame);
+	}
+
+	this.getTeamData = function(team) {
+
+		var deferred = $q.defer();
+		var url = 'https://api.parse.com/1/classes/' + team + '?order=-createdAt';
+		$http.get(url).then(function(data) {
+			var results = data.data.results;
+			var wins = 0;
+			var losses = 0;
+			for (var i = 0; i < results.length; i++) {
+
+				if (results[i].won === true) {
+					wins++;
+				} else if (results[i].won === false) {
+					losses++;
+				}
+			}
+			results.wins = wins;
+			results.losses = losses;
+			deferred.resolve(results);
+		})
+		return deferred.promise;
+	}
 
 });
